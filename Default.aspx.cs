52c90757808dce69e214f89e45bc1aa4c5285d79
anh do <dodinhanh195@gmail.com>
2017-11-04 10:17:45 +0700

    Add insert customer function

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -16,7 +16,35 @@ public partial class _Default : System.Web.UI.Page
     }
     protected void btnCreateNew_Click(object sender, EventArgs e)
     {
-       
+        DateTime birthday;
+        if (!DateTime.TryParse(txtBirthday.Text, out birthday))
+        {
+            Response.Write("<script>alert('Datetime is not valid')</script>");
+            return;
+        }
+
+        if ((int)DataProvider.ExcuteScalar("SELECT COUNT(*) FROM Customer WHERE Id = '" + txtID.Text.Trim() + "'") > 0)
+        {
+            Response.Write("<script>alert('The id was existing')</script>");
+            return;
+        }
+
+        string query = "INSERT INTO Customer VALUES('"
+            + txtID.Text + "', N'"
+            + txtName.Text + "', '"
+            + birthday + "', N'"
+            + txtCompanyName.Text + "', '"
+            + txtPhoneNumber.Text + "', N'"
+            + ddlCountry.SelectedValue + "')";
+
+        if (DataProvider.ExcuteNonQuery(query))
+        {
+            txtName.Text = txtID.Text = txtBirthday.Text = txtCompanyName.Text = txtPhoneNumber.Text = string.Empty;
+            ddlCountry.SelectedIndex = -1;
+            return;
+        }
+
+       Response.Write("<script>alert('ERROR: Could not create new customer information')</script>");
     }
 
     public void LoadDropDownListCountry()
@@ -26,4 +54,6 @@ public partial class _Default : System.Web.UI.Page
         ddlCountry.DataTextField = "Name";
         ddlCountry.DataBind();
     }
+
+   
 }
