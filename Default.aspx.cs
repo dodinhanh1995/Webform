74e6fedd46a1a198a001b9ea3792b8045d674836
anh do <dodinhanh195@gmail.com>
2017-11-04 11:08:40 +0700

    Add updating, editting, canceling events

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -12,8 +12,10 @@ public partial class _Default : System.Web.UI.Page
         if (!IsPostBack)
         {
             LoadDropDownListCountry();
+            LoadData();
         }
     }
+
     protected void btnCreateNew_Click(object sender, EventArgs e)
     {
         DateTime birthday;
@@ -39,6 +41,7 @@ public partial class _Default : System.Web.UI.Page
 
         if (DataProvider.ExcuteNonQuery(query))
         {
+            LoadData();
             txtName.Text = txtID.Text = txtBirthday.Text = txtCompanyName.Text = txtPhoneNumber.Text = string.Empty;
             ddlCountry.SelectedIndex = -1;
             return;
@@ -55,5 +58,59 @@ public partial class _Default : System.Web.UI.Page
         ddlCountry.DataBind();
     }
 
-   
+    public void LoadData()
+    {
+        gvCustomer.DataSource = DataProvider.ExcuteQuery("SELECT c.*, ct.Name as CountryName " +
+                                                        "FROM Customer as c " + 
+                                                        "INNER JOIN Country as ct " +
+                                                        "ON c.CountryId = ct.Id");
+        gvCustomer.DataBind();
+    }
+
+    protected void gvCustomer_RowUpdating(object sender, GridViewUpdateEventArgs e)
+    {
+        string id = gvCustomer.DataKeys[e.RowIndex].Value.ToString();
+        string name = ((TextBox)gvCustomer.Rows[e.RowIndex].FindControl("txtEditName")).Text;
+        string birthday = ((TextBox)gvCustomer.Rows[e.RowIndex].FindControl("txtEditBirthday")).Text;
+        string companyName = ((TextBox)gvCustomer.Rows[e.RowIndex].FindControl("txtEditCompanyName")).Text;
+        string phoneNumber = ((TextBox)gvCustomer.Rows[e.RowIndex].FindControl("txtEditPhoneNumber")).Text;
+        string countryId = ((DropDownList)gvCustomer.Rows[e.RowIndex].FindControl("ddlEditCountry")).SelectedValue;
+
+        string query = "UPDATE Customer SET Name = N'"
+            + name + "', Birthday = '"
+            + birthday + "', CompanyName = N'"
+            + companyName + "', PhoneNumber = '"
+            + phoneNumber + "', CountryId = N'"
+            + countryId + "'"
+            + " WHERE Id = '" + id + "'";
+
+        if (DataProvider.ExcuteNonQuery(query))
+        {
+            gvCustomer.EditIndex = -1;
+            LoadData();
+            return;
+        }
+
+        Response.Write("<script>alert('ERROR: Could not create new customer information')</script>");
+    }
+
+    protected void gvCustomer_RowEditing(object sender, GridViewEditEventArgs e)
+    {
+        gvCustomer.EditIndex = e.NewEditIndex;
+        LoadData();
+    }
+    protected void gvCustomer_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+    {
+        gvCustomer.EditIndex = -1;
+        LoadData();
+    }
+
+    protected void cbDeleteAll_CheckedChanged(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnDelete_Click(object sender, EventArgs e)
+    {
+
+    }
 }
